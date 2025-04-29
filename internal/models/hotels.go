840f6ebbe300953dd1b67ef1294ff75198fd4fec
Gopgap <Noppawat3984@gmail.com>
2025-04-29 17:25:48 +0700

    remove id and replace to gorm model

--- internal/models/hotels.go
+++ internal/models/hotels.go
@@ -1,17 +1,17 @@
 package models
 
-import "time"
+import (
+	"gorm.io/gorm"
+)
 
 type Hotel struct {
-	ID          uint      `gorm:"primaryKey"`
-	Name        string    `gorm:"not null"`
-	Description string    `gorm:"default:null"`
-	Type        string    `gorm:"default:null"`
-	Price       float64   `gorm:"not null"`
-	RoomsQty    int       `gorm:"not null"`
-	Active      bool      `gorm:"default:true"`
-	CreatedAt   time.Time `gorm:"default:current_timestamp"`
-	UpdatedUp   time.Time `gorm:"default:current_timestamp"`
+	gorm.Model
+	Name        string  `gorm:"not null"`
+	Description string  `gorm:"default:null"`
+	Type        string  `gorm:"default:null"`
+	Price       float64 `gorm:"not null"`
+	RoomsQty    int     `gorm:"not null"`
+	Active      bool    `gorm:"default:true"`
 }
 
 type HotelFacility struct {
